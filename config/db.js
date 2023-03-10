433032da85ca306ac6424a2daf3e42d569a7fc5e
shreyas4545 <shreyasjakati66@gmail.com>
2023-03-10 19:10:53 +0530

    Changes

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config();
-const mongourl=process.env.MONGO_URL|| "mongodb://localhost:27017/TelegramBot"
+const mongourl=process.env.MONGO_URL|| "mongodb+srv://Shreyas4545:20169361@cluster0.1npzh7f.mongodb.net/?retryWrites=true&w=majority"
 
 export const connectDB=mongoose.connect(mongourl,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
     console.log("DB Connected Successfully");
-}).catch(() =>{
+}).catch((err) =>{
     console.log("DB Connection Failed");
     console.log(err);   
     process.exit(1);
